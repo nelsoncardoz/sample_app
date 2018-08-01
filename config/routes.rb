3a97f2fc77aaa389de58446034f9643a08f2f16b
Nelson Cardoz <nelson.cardoz@gmail.com>
2018-08-01 10:54:58 +0100

    4.1

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   resources :products
+  resources	:orders, only: [:index, :show, :create, :destroy] 
+  get 'products/index'
   get 'simple_pages/about'
   get 'simple_pages/contact'
   get 'simple_pages/index'
