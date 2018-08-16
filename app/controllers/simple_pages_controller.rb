7ba451e91e35d53f3eb681555a47867285101a95
Nelson Cardoz <nelson.cardoz@gmail.com>
2018-08-16 09:52:35 +0100

    4.5

--- app/controllers/simple_pages_controller.rb
+++ app/controllers/simple_pages_controller.rb
@@ -1,9 +1,6 @@
 class SimplePagesController < ApplicationController
-  def index
-  end
-
-  def landing_page
-    @featured_product = Product.first
-    
-  end
-end
+   def landing_page
+     @featured_product = Product.first
+     @products = Product.all
+   end
+ end
